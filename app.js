b56f8de8a6cdab55df5b6fe4f5bc79814c267645
ilnur minagulov <ilnurminagulov@yandex.ru>
2022-11-01 18:37:48 +0300

    End of code

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 const cols = document.querySelectorAll('.col')
 
+
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+        if(event.code.toLowerCase() == 'space'){
+            setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) =>{
+    const type = event.target.dataset.type
+
+    if(type == 'lock'){
+        const node = event.target.tagName.toLowerCase() =='i'
+            ? event.target
+            : event.target.children [0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }   else if(type=='copy'){
+        copyToClickboard(event.target.textContent)
+    }
+}) 
+
+function copyToClickboard(text){
+return navigator.clipboard.writeText(text)
+}
+
 //RGB
 //#FF0000   КРАСНЫЙ
 //#00FF00   ЗЕЛЕНЫЙ 
@@ -16,12 +43,25 @@ function generateRandomColor(){
 
 function setRandomColors() {
     cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector ('h2')
-        const color = generateRandomColor()
-
+        const button = col.querySelector ('button')
+        const color = chroma.random()
+        
+        if (isLocked){
+            return
+        }
+       
         text.textContent =color
         col.style.background = color
+        
+         setTextColor(text, color)
+         setTextColor(button, color)
     } )
-}
+} 
 
+function setTextColor(text, color) {
+   const luminance =  chroma(color).luminance()
+   text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
 setRandomColors()
